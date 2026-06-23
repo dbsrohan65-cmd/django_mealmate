f1bcc381106f577630433a99a0fd2b8c119410eb
dbsrohan65-cmd <dbsrohan65@gmail.com>
2026-06-23 12:32:11 +0530

    Configured static files for Render

--- mealmate/settings.py
+++ mealmate/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'delivery/static',
+]
 
 RAZORPAY_KEY_ID = 'rzp_test_T2CcyEkTFglKsL'
 RAZORPAY_KEY_SECRET = 'VniEh2xVSoPNkd1dem8VmggJ'
